cff310c0a2b2d3f821644014fee2e73662c789c6
Lulzasaur <frost9192@gmail.com>
2018-08-07 22:45:37 -0700

    second commit

---
+++ meme.js
@@ -0,0 +1,10 @@
+var img = document.getElementById('img'),
+    btmtxt = document.getElementById('btmtxt'),
+    toptxt = document.getElementById('toptxt'),
+    button = document.getElementById('button')
+
+button.addEventListener('click',function(){
+    console.log(img.value);
+});
+    
+
